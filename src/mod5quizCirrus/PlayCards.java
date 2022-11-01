0d772b90d1cd6181a48dc6e50b2d189b5ff7efa5
tricaratops <116924844+tricaratops@users.noreply.github.com>
2022-10-31 21:18:25 -0400

    I added a point calculator and fleshed out the round to print all three scores.

--- src/mod5quizCirrus/PlayCards.java
+++ src/mod5quizCirrus/PlayCards.java
@@ -48,7 +48,11 @@ public class PlayCards {
 		System.out.println("***Lets start the game***");
 		//for(int i=0; i<17; i++) {
 		System.out.println(p1.toString());
-		
+		System.out.println("Value: " + points(handOne.getCards().get(0)));
+		System.out.println(p2.toString());
+		System.out.println("Value: " + points(handTwo.getCards().get(0)));
+		System.out.println(comp.toString());
+		System.out.println("Value: " + points(handComp.getCards().get(0)));
 		
 		//System.out.println("\nShuffled cards:");
 		//System.out.println(doc.toString());
@@ -79,6 +83,23 @@ public class PlayCards {
 		return hand;
 				
 	}
+	
+	//This calculates the total number of points, work in progress.  -Cara
+	public static int points(Card c) {
+		int value = c.getValue();
+		String face = c.getSuit();
+		int faceBonus = 0;
+		if (face.equalsIgnoreCase("hearts"))
+			faceBonus = 4;
+		else if (face.equalsIgnoreCase("diamonds"))
+			faceBonus = 3;
+		else if (face.equalsIgnoreCase("clubs"))
+			faceBonus = 2;
+		else
+			faceBonus = 1;
+		return value + faceBonus;
+	}
+	
 	
 	/* prints the beginning of the sample output 
 	 change it if needed to fit with program - Eduardo */
--- src/mod5quizCirrus/readme.txt
+++ src/mod5quizCirrus/readme.txt
@@ -7,6 +7,8 @@ Please change my code if needed.
 
 Cara Eskew:
 Comments:
+10/31/22 - I have been working on fleshing out the round to print all players and to print the total value.  The
+points aren't calculating correctly yet but I will resolve that.
 
 Benjamin Wheatley:
 Comments:
