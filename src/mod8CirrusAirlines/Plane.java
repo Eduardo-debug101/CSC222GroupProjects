76a314d0937da9790aeac40eb582696ad68fbd61
Eduardo-debug101 <eja2770@email.vccs.edu>
2022-12-08 09:11:33 -0500

    Small 2d array printing fix

--- src/mod8CirrusAirlines/Plane.java
+++ src/mod8CirrusAirlines/Plane.java
@@ -17,13 +17,30 @@ public class Plane {
 	// Need some work. Should handle rows with different columns numbers. - Eduardo 12/7
 	public void printArray() {
 		System.out.println("Here are the seats in this plane. X's means reserved and O's mean available.");
-		for (int row = 0; row < seats.length; row++) {
+		for (int row = 0; row < 2; row++) {
 			for (int col = 0; col < seats[0].length; col++)
-				if (seats[row][col] == null) {
+				if (seats[0][col] == null || seats[1][col] == null) {
 					System.out.print("O" + "   ");
-				} else {
+				} 
+//				else if (seats[2][col] == null || seats[3][col] == null) {
+//					System.out.print("O" + "   ");
+//				}
+				else {
 					System.out.print(seats[row][col].getSeat());
 				}
+			System.out.println();
+		}
+		System.out.println();
+		
+		for (int row = 0; row < 2; row++) {
+			for (int col = 0; col < seats[2].length; col++)
+				if (seats[2][col] == null || seats[3][col] == null) {
+					System.out.print("O" + "   ");
+				}
+				else {
+					System.out.print(seats[row][col].getSeat());
+				}
+			System.out.println();
 		}
 		System.out.println();
 	}
